e938cca0a7b067acf044625543babf7134b08150
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2025-03-05 21:14:47 +0530

    chore: already made backend

--- dashboard/.open-next/assets/_next/static/chunks/app/dashboard/orders/page-1d8b1eb6abf60977.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[857],{9931:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,1189)),Promise.resolve().then(s.bind(s,2948))}},function(e){e.O(0,[189,972,971,117,744],function(){return e(e.s=9931)}),_N_E=e.O()}]);
