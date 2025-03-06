d2aedf1b8773f8761ebff32e01f34678b6fa5aa8
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2025-03-06 12:41:28 +0530

    chore: products details screen

--- ecommerce-mobile/app/product/[id].tsx
+++ ecommerce-mobile/app/product/[id].tsx
@@ -1,45 +1,46 @@
+import React from 'react'
+import { Text } from '@/components/ui/text'
+import { useLocalSearchParams } from 'expo-router'
+import products from '@/assets/products.json'
+import { VStack } from '@/components/ui/vstack'
 import { Box } from "@/components/ui/box";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Image } from "@/components/ui/image";
-import { Text } from "@/components/ui/text";
-import { VStack } from "@/components/ui/vstack";
-import { Link } from "expo-router";
 
-export type TProductCard =
-    {
-        "id": number,
-        "name": string,
-        "description": string,
-        "image": string,
-        "price": number
+
+
+const details = () => {
+    const { id } = useLocalSearchParams()
+
+    const product = products.find(p => p.id === Number(id))
+
+    if (!product) {
+        return <Text>Product not found</Text>
     }
 
-function ProductCard({ id, name, description, image, price }: TProductCard) {
     return (
-        <Card className="p-5 rounded-lg max-w-[360px] flex-1">
-            <Link href={`/product/${id}`}>
-                <Image
-                    source={{
-                        uri: image
-                    }}
-                    className="mb-6 h-[240px] w-full rounded-md "
-                    alt={name}
-                    resizeMode="contain"
-                />
-            </Link>
+        <Card className="p-5 rounded-lg max-w-[560px] flex-1">
+            <Image
+                source={{
+                    uri: product.image
+                }}
+                className="mb-6 h-[240px] w-full rounded-md "
+                alt={product.name}
+                resizeMode="contain"
+            />
             <Text
                 className="text-sm font-normal mb-2 text-typography-700"
             >
-                {name}
+                {product.name}
             </Text>
             <VStack className="mb-6">
                 <Heading size="md" className="mb-4">
-                    {price}
+                    {product.price}
                 </Heading>
                 <Text size="sm">
-                    {description}
+                    {product.description}
                 </Text>
             </VStack>
             <Box
@@ -63,7 +64,7 @@ function ProductCard({ id, name, description, image, price }: TProductCard) {
                 </Button>
             </Box>
         </Card>
-    );
+    )
 }
 
-export default ProductCard
+export default details
